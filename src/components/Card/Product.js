47abfd1b431d294e73c432761ac2951cdc31761c
Sebastian Hernandez <sebashdm@gmail.com>
2021-06-22 09:43:14 -0500

    grid cards styles are added

--- src/components/Card/Product.js
+++ src/components/Card/Product.js
@@ -16,21 +16,23 @@ const Product = ({
   id,
 }) => {
   return (
-    <Card color="blue">
-      <Image src={image} wrapped ui={false} />
-      <Card.Content>
-        <Card.Header>{category}</Card.Header>
-        <Card.Meta>
-          <span className="date">{name}</span>
-        </Card.Meta>
-        <Card.Meta>
-          <span className="date">
-            {" "}
-            <Icon name="money">Cost:{cost}</Icon>
-          </span>
-        </Card.Meta>
-      </Card.Content>
-    </Card>
+    <div className="card">
+      <Card color="blue">
+        <Image src={image} wrapped ui={false} />
+        <Card.Content>
+          <Card.Header>{category}</Card.Header>
+          <Card.Meta>
+            <span className="date">{name}</span>
+          </Card.Meta>
+          <Card.Meta>
+            <span className="date">
+              {" "}
+              <Icon name="money">Cost:{cost}</Icon>
+            </span>
+          </Card.Meta>
+        </Card.Content>
+      </Card>
+    </div>
   );
 };
 
